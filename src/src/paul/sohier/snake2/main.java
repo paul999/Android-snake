0075017710eeebd71431f439ef7e827b557e4a00
Paul Sohier <paul999@phpbb.com>
2010-11-07 16:52:37 +0100

    File

--- src/src/paul/sohier/snake2/main.java
+++ src/src/paul/sohier/snake2/main.java
@@ -1,5 +1,7 @@
 package paul.sohier.snake2;
 
+import com.admob.android.ads.AdView;
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -9,5 +11,13 @@ public class main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+		AdView ad = (AdView) findViewById(R.id.adver);
+		ad.bringToFront();
+		
+//		  AdManager.setTestDevices( new String[] {                 
+//				     AdManager.TEST_EMULATOR,             // Android emulator
+//				     "0BD8378A2247D33B57762EB03AF750D7",  // My T-Mobile G1 Test Phone
+//				     } );          
     }
 }
